4befb98f70b074125957a94b02e2f21fc12a5f8a
mahlet <mahlet2ambaye@gmail.com>
2026-03-29 11:20:00 +0100

    feat:add sample task

--- week1 practice/storage.py
+++ week1 practice/storage.py
@@ -4,6 +4,22 @@ from typing import Dict
 from model import Task
 
 
-tasks: Dict[int, Task] = {}
+tasks: Dict[int, Task] = {
+    1: Task(
+        id=1,
+        title="Sample Task",
+        description="This is a sample task",
+        completed=False,
+        created_at="2024-06-01T12:00:00Z",
+    ),
+    2: Task(
+        id=2,
+        title="Another Task",
+        description="This is another sample task",
+        completed=True,
+        created_at="2024-06-02T12:00:00Z",
+    ),
+}
 
-current_id:int = 0
+
+current_id: int = tasks.__len__() - 1
